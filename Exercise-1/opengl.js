ffcb6ea8949f84b8e0b096c9df0957181f6d09ee
kwanele-Cyber <52756699+kwanele-Cyber@users.noreply.github.com>
2024-10-23 11:33:17 +0200

    fix_code_bugs

--- Exercise-1/opengl.js
+++ Exercise-1/opengl.js
@@ -1,2 +1,16 @@
-const canvas= document.getElementById("canvas");
-const ctx = convas.getContext("webgl2");
+main();
+
+function main()
+{
+    const canvas= document.getElementById("canvas");
+    const gl = convas.getContext("webgl");
+
+    if(lg === null){
+        alert("Unable to initiate WebGL. Your browser or Machine may not support it.",);
+        return;
+    }
+
+    //set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLO_BUFFER_BIT);
+}
